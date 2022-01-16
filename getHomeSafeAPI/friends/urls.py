71d5399c827058742a252b6c1ae316a59c18190b
Wilson Tu <s31302@gmail.com>
2022-01-15 21:51:16 -0800

    friends url

---
+++ getHomeSafeAPI/friends/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+from .views import FriendsCreateViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = routers.DefaultRouter()
+router.register(r'friends', FriendsCreateViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_friends'))
+]
