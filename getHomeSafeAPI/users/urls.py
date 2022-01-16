b464ec8dd32c5e5e779a76f37724d68f643183ff
Wilson Tu <s31302@gmail.com>
2022-01-15 20:18:35 -0800

    add multiple views

--- getHomeSafeAPI/users/urls.py
+++ getHomeSafeAPI/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-from . import views
+from .views import CreateUserAPIView, LogoutUserAPIView, CustomObtainAuthToken, UserCreateViewSet
+from django.urls import re_path
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserCreateViewSet)
+router.register(r'users', UserCreateViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -11,3 +13,22 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns = [
+    re_path(r'^auth/login/$',
+        CustomObtainAuthToken.as_view(),
+        name='auth_user_login'),
+    re_path(r'^auth/register/$',
+        CreateUserAPIView.as_view(),
+        name='auth_user_create'),
+    re_path(r'^auth/logout/$',
+        LogoutUserAPIView.as_view(),
+        name='auth_user_logout'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
+]
+
+
+
+
