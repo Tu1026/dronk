26d67dc86c72025b422863feb629346cbad2c7c0
Wilson Tu <s31302@gmail.com>
2022-01-15 21:52:56 -0800

    correct urls in users

--- getHomeSafeAPI/users/urls.py
+++ getHomeSafeAPI/users/urls.py
@@ -9,10 +9,10 @@ router.register(r'users', UserCreateViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('api-auth/', include('rest_framework.urls', namespace='rest_users'))
+# ]
 
 urlpatterns = [
     re_path(r'^auth/login/$',
