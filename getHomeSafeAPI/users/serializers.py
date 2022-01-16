bcbb50593e37f5120455d54554a1de98c462f5f6
Wilson Tu <s31302@gmail.com>
2022-01-15 16:58:13 -0800

    update the serializer for user

--- getHomeSafeAPI/users/serializers.py
+++ getHomeSafeAPI/users/serializers.py
@@ -1,1 +1,21 @@
-from django.contrib.auth.models
+from rest_framework import serializers
+from django.contrib.auth import get_user_model # If used custom user model
+
+UserModel = get_user_model()
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+ 
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = UserModel.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+        )
+        return user
+
+    class Meta:
+        model = UserModel
+        # Tuple of serialized model fields (see link [2])
+        fields = "__all__"
