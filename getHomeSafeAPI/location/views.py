45f7ce368a03de8daf4e43d569cc176c95698f64
Wilson Tu <s31302@gmail.com>
2022-01-16 03:30:30 -0800

    geoloc view

--- getHomeSafeAPI/location/views.py
+++ getHomeSafeAPI/location/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from .models import Location
+from .serializers import LocationSerializer
+from django.conf import settings
 
 # Create your views here.
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    permission_claases = [IsAuthenticated,]
+    
+    def retrieve(self, request, *args, **kwargs):
+        locations = get_list_or_404(self.queryset, user_id=kwargs["pk"])
+        serialzer = LocationSerializer(locations, many=True)
+        # print(serialzer.data)
+        return Response(serialzer.data)
