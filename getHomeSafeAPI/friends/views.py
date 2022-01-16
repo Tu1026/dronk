f82da2ba8bbb600f02b2ec06db88267b809a8330
Wilson Tu <s31302@gmail.com>
2022-01-15 21:51:27 -0800

    friends viewsetr

--- getHomeSafeAPI/friends/views.py
+++ getHomeSafeAPI/friends/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from .models import Friends
+from .serializers import FriendsSerializer
+
 
 # Create your views here.
+class FriendsCreateViewSet(viewsets.ModelViewSet):
+    queryset = Friends.objects.all()
+    serializer_class = FriendsSerializer
+    permission_claases = [IsAuthenticated,]
