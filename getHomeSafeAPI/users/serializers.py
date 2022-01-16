edca27659d57c83493bc7ba39b858cff47824bdb
Wilson Tu <s31302@gmail.com>
2022-01-15 20:18:28 -0800

    Include multiple serializers

--- getHomeSafeAPI/users/serializers.py
+++ getHomeSafeAPI/users/serializers.py
@@ -5,17 +5,32 @@ UserModel = get_user_model()
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
- 
-    password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
         user = UserModel.objects.create_user(
             email=validated_data['email'],
-            password=validated_data['password'],
+            password=validated_data['password']
         )
+        user.save()
         return user
 
     class Meta:
         model = UserModel
-        # Tuple of serialized model fields (see link [2])
-        fields = "__all__"
+        fields = ("url", "email", "first_name", "last_name", "password")
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True,
+                                     style={'input_type': 'password'})
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password', 'first_name', 'last_name')
+        write_only_fields = ('password')
+        read_only_fields = ('is_staff', 'is_superuser', 'is_active',)
+
+    def create(self, validated_data):
+        user = super(CreateUserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
