1e3266b7bd4ae8e12a9ec0495050fbd9eb3ac132
tu1026 <s31302@gmail.com>
2022-01-16 12:22:05 -0800

    adding default secret

--- getHomeSafeAPI/getHomeSafeAPI/settings.py
+++ getHomeSafeAPI/getHomeSafeAPI/settings.py
@@ -14,13 +14,13 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+import secrets
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("API_KEY")
+SECRET_KEY = os.getenv("API_KEY") if os.getenv("API_KEY") else secrets.token_urlsafe()
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
