870450e67b8007b6c286858de29e4d372444ab16
Wilson Tu <s31302@gmail.com>
2022-01-15 16:57:57 -0800

    create user viewset

--- getHomeSafeAPI/users/views.py
+++ getHomeSafeAPI/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserCreateViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_claases = [AllowAny]
+    # def get_permissions(self):
+    #     """
+    #     Instantiates and returns the list of permissions that this view requires.
+    #     """
+    #     if self.action == 'list':
+    #         permission_classes = [IsAuthenticated]
+    #     else:
+    #         permission_classes = [AllowAny]
+    #     return [permission() for permission in permission_classes]
