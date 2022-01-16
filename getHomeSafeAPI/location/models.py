fbbb7c02a4a10c1f2940d69c40f1b8b278ae8167
Wilson Tu <s31302@gmail.com>
2022-01-16 03:30:56 -0800

    geo loc models

--- getHomeSafeAPI/location/models.py
+++ getHomeSafeAPI/location/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+class Location(models.Model):
+    
+    latitude = models.FloatField(max_length=30)
+    longitude = models.FloatField(max_length=30)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{User.objects.get(pk=self.user_id)}'s location"
