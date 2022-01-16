20cd1f7dd040987271fefca749a115e5f4642ce9
Wilson Tu <s31302@gmail.com>
2022-01-16 03:31:02 -0800

    geo loc urls

--- getHomeSafeAPI/getHomeSafeAPI/urls.py
+++ getHomeSafeAPI/getHomeSafeAPI/urls.py
@@ -33,5 +33,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-    path('', include('friends.urls'))
+    path('', include('friends.urls')),
+    path('', include('location.urls'))
 ]
