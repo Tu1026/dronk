e91f4edf7944bbb1dc1b12827d16af6b1e7ae420
Wilson Tu <s31302@gmail.com>
2022-01-15 21:51:00 -0800

    friends models

--- getHomeSafeAPI/friends/models.py
+++ getHomeSafeAPI/friends/models.py
@@ -1,8 +1,10 @@
 from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-from users import User
+from django.apps import apps
+from django.conf import settings
 
+User = settings.AUTH_USER_MODEL
 class Friends(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -13,6 +15,3 @@ class Friends(models.Model):
         return f"{User.objects.get(pk=self.user_id)}'s friends"
     
     # notice the absence of a "Password field", that is built in.
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [] # Email & Password are required by default.
